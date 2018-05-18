43d656d9aa7626e3d86ea6fff9261e824a1a9590
asinha94 <asinha94@gmail.com>
2018-05-18 15:17:31 -0700

    added initial power board

--- ros/src/peripherals/src/power_board.cpp
+++ ros/src/peripherals/src/power_board.cpp
@@ -3,22 +3,23 @@
 #include <memory>
 #include <serial/serial.h>
 
-#include "peripherals/motor.h"
-#include "peripherals/motors.h"
 #include "monitor/GetSerialDevice.h"
+#include "peripherals/powerboard.h"
 
 using rosserv = ros::ServiceServer;
+using powerboardInfo = peripherals::powerboard;
 
 class power_board{
 public:
-    power_board(std::string port, int baud_rate = 9600, int timeout = 1000);
+    power_board(const std::string & port, int baud_rate = 9600, int timeout = 1000);
     ~power_board();
+    void get_powerboard_data(powerboardInfo & msg);
 private:
     std::unique_ptr<serial::Serial> connection = nullptr;
-    std::string write(std::string out, bool ignore_response = true, std::string eol = "\n");
+    std::string write(const std::string & out, bool ignore_response = true, std::string eol = "\n");
 };
 
-power_board::power_board(std::string port, int baud_rate, int timeout) {
+power_board::power_board(const std::string & port, int baud_rate, int timeout) {
     ROS_INFO("Connecting to power_board on port: %s", port.c_str());
     connection = std::unique_ptr<serial::Serial>(new serial::Serial(port, (u_int32_t) baud_rate, serial::Timeout::simpleTimeout(timeout)));
 }
@@ -27,7 +28,7 @@ power_board::~power_board() {
     connection->close();
 }
 
-std::string power_board::write(std::string out, bool ignore_response, std::string eol)
+std::string power_board::write(const std::string & out, bool ignore_response, std::string eol)
 {
     connection->write(out + eol);
     ROS_INFO("%s", out.c_str());
@@ -36,6 +37,21 @@ std::string power_board::write(std::string out, bool ignore_response, std::strin
     }
     return connection->readline(65536ul, eol);
 }
+
+void power_board::get_powerboard_data(powerboardInfo &msg) {
+    std::string currents = this->write("CRA", false);
+    std::string voltages = this->write("VTA", false);
+    std::string temperature = this->write("TMP", false);
+    std::string humidity = this->write("HUM", false);
+    std::string water = this->write("WTR", false);
+    std::string pressure_internal = this->write("PIN");
+    std::string pressure_external = this->write("PEX");
+
+    msg.current_all = 50;
+    msg.current_battery_1 = 50;
+    msg.current_battery_2 = 50;
+    msg.current_motors = 50;
+} 
 
 int main(int argc, char ** argv)
 {
