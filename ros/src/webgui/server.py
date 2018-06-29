6e4bd57ca341e108a2e1a5ba398282797553746a
asinha94 <asinha94@gmail.com>
2018-06-28 19:01:30 -0700

    caught sigint instead of keyboard exception so that port is closed cleanly

--- ros/src/webgui/server.py
+++ ros/src/webgui/server.py
@@ -1,14 +1,21 @@
 #!/usr/bin/python
 import SimpleHTTPServer
 import SocketServer
+import signal
+import sys
 import os
 
-if __name__ == "__main__":
-    PORT = 12345
-    Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
-    httpd = SocketServer.TCPServer(("0.0.0.0", PORT), Handler)
+PORT = 12345
+Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
+httpd = SocketServer.TCPServer(("0.0.0.0", PORT), Handler)
+
+def signal_handler(signal, frame):
+    httpd.server_close()
+    sys.exit(0)
+    
+if __name__ == '__main__':
     os.chdir(os.path.dirname(__file__))
-    try:
-        httpd.serve_forever()
-    finally:
-        httpd.server_close()
+    signal.signal(signal.SIGINT, signal_handler)
+    httpd.serve_forever()
+        
+        
