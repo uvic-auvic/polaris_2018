e927c026312da334c788c3dc0065cb1170e41847
auvic <auvic@uvic.ca>
2018-07-14 01:29:18 +0000

    added more ros deps for gscam

--- configuration/jetsonSetup.sh
+++ configuration/jetsonSetup.sh
@@ -5,6 +5,7 @@
 # Anything CUDA related or NVIDIA related should be done using JetPack
 
 sudo apt-get install -y gstreamer1.0-tools libgstreamer1.0-dev libgstreamer-plugins-good1.0-dev libgstreamer-plugins-base1.0-dev
+sudo apt-get install -r ros-kinetic-camera-calibration-parsers ros-kinetic-camera-info-manager
 ./common-packages.sh
 pushd ../ros/src
 git clone https://github.com/ros-drivers/gscam
